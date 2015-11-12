560fa1b0d84da41b5e83762c9844376d7f81ef50
Frazer Hogg <frazerj@me.com>
2015-11-12 22:08:47 +0000

    Table contents sorted in order of latest to oldest created.

--- StudentTableViewController.swift
+++ StudentTableViewController.swift
@@ -13,7 +13,7 @@ class StudentTableViewController: UITableViewController {
     //MARK: - Properties
     
     let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-    
+    var sortedStudentData = [StudentInformation]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,69 +22,27 @@ class StudentTableViewController: UITableViewController {
         let adjustForTabBar = UIEdgeInsetsMake(0, 0, CGRectGetHeight(tabBarController!.tabBar.frame), 0)
         tableView.contentInset = adjustForTabBar
         
+        //Sort the student data so that it is in order of latest to oldest created
+        sortedStudentData = appDelegate.studentData.sort() {$0.createdAt.compare($1.createdAt) == NSComparisonResult.OrderedDescending}
     }
 
     // MARK: - Table view data source
 
+    ///Function for defining the number of rows the table should have.
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return appDelegate.studentData.count
     }
 
+    ///Function for defining the contents of each row
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCellWithIdentifier("studentCell")!
         
-        let student = appDelegate.studentData[indexPath.row]
+        let student = sortedStudentData[indexPath.row]
         
         cell.textLabel?.text = student.firstName + " " + student.lastName
         cell.detailTextLabel!.text = student.mediaURL
 
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
